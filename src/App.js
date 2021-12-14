90deab4a2e09dd261d54f51e2c010a222728889b
Tony <huydao2802@gmail.com>
2021-12-14 16:57:55 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bulma/css/bulma.min.css';
+import Web3 from 'web3';
+import { useState,useEffect } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
 function App() {
+
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        })
+      } else {
+        console.error("Please, install Metamask")
+      }
+    }
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    }
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          <p>Current Balance: <strong> 10 ETH</strong></p>
+        </div>
+        <button className="button is-primary mr-5 ml-5">Donate</button>
+        <button className="button is-danger mr-5">Withdraw</button>
+        <button className="button is-link" onClick={(() =>
+          web3Api.provider.request({ method: "eth_requestAccounts"})
+          )}>Connect Wallet</button>
+        <span className="">
+          <p>
+            <strong>Accounts Addresses: </strong>
+            {account ? account : "Account Denied"}
+          </p>
+        </span>
+      </div>
     </div>
   );
 }
